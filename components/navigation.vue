cef919da4f5dce8e7e92865b7848a55858447615
Felix Terpstra <felix@felixterpstra.com>
2020-09-24 07:37:26 +0100

    add navigation (not working)

---
+++ components/navigation.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="$style.container">
+    <div class="width-site">
+      <div :class="$style.logo" />
+      <div :class="$style.link">contact</div>
+      <div :class="$style.link">practise</div>
+      <div :class="$style.link">commercial</div>
+      <div :class="$style.link">residential</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style module lang='scss'>
+@import '@/assets/styles/index.scss';
+
+.container {
+  transition: 70ms ease-in-out;
+  display: block;
+  margin: 0 auto;
+  width: 100%;
+  background: white;
+  height: 80px;
+  z-index: 400;
+  position: fixed;
+  top: 0;
+}
+
+.logo {
+  transition: 70ms ease-in-out;
+  margin-top: 28px;
+  background: url('~assets/images/logo.svg') 0 0 / 148px 22px;
+  width: 148px;
+  height: 22px;
+  float: left;
+}
+
+.link {
+  margin-top: 31px;
+  float: right;
+  text-transform: uppercase;
+  color: $text-nav;
+  font-size: 13px;
+  font-weight: 600;
+  letter-spacing: 2.6px;
+  margin-left: 38px;
+  cursor: pointer;
+  transition: 100ms all;
+
+  &:hover {
+    color: $color-primary;
+  }
+
+  &.active {
+    color: $color-primary;
+    border-bottom: 2px solid #241b12;
+    padding-bottom: 6px;
+    position: relative;
+    bottom: 2px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>
-    <Nuxt />
+  <div :class="$style.site">
+    <div :class="$style.navigationContainer">
+      <Navigation />
+    </div>
+    <div :class="$style.pageContainer">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
-<style>
+<script>
+import Vue from 'vue'
+import Navigation from '@/components/navigation'
+
+export default Vue.extend({
+  components: {
+    Navigation,
+  },
+})
+</script>
+
+<style module lang='scss'>
+.site {
+
+}
+
+.navigationContainer {
+
+}
+
+.pageContainer {
+  margin-top: 80px;
+}
 </style>
