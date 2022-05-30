36518b28fe80758e285c12c823c4818d001d09a1
lowrise <lowrise@users.noreply.github.com>
2022-05-30 21:10:49 +0000

    deploy: 1c53b136d119a2bc9d07165b0c29eba04027ab13

--- _static/static/1653944378/projects/no7-balmac/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1653944378",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fno7-balmac",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
