844d0672c88d661ad9f4960004452ef3616e5e4d
lowrise <lowrise@users.noreply.github.com>
2022-05-30 20:30:15 +0000

    deploy: 656b68a490770f0d294ba2ab446ca9837b45eb73

--- _static/static/1616580830/projects/city-apartment/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1616580830",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fcity-apartment",config:{app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
