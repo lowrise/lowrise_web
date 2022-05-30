e84f25e7b2c016046d23ec6edb1aac2113c7d87f
lowrise <lowrise@users.noreply.github.com>
2022-05-30 21:13:23 +0000

    deploy: 1c53b136d119a2bc9d07165b0c29eba04027ab13

---
+++ _static/static/1653945202/projects/the-good-oil-cafe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1653945202",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fthe-good-oil-cafe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
