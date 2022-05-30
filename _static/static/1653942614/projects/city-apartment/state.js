5bbe3130129579508637f8af5c6852a6a1b302b0
lowrise <lowrise@users.noreply.github.com>
2022-05-30 20:40:28 +0000

    deploy: 88a3f9df15203d6c7d695118ea5c102c62537297

--- _static/static/1653942614/projects/city-apartment/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1653942614",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fcity-apartment",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
--- _static/static/1653942614/projects/maori-hill-house-1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1653942614",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fmaori-hill-house-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
