e11582ef2c0da548a1d00d35bb8877a4866c7a5a
lowrise <lowrise@users.noreply.github.com>
2022-05-31 14:34:35 +0000

    deploy: 1c53b136d119a2bc9d07165b0c29eba04027ab13

--- _static/static/1654007655/thanks/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1654007655",layout:"default",error:a,serverRendered:true,routePath:"\u002Fthanks",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
