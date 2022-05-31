d6e693d5e33f7cd5feffe560a1e079d826e365ad
lowrise <lowrise@users.noreply.github.com>
2022-05-31 14:39:33 +0000

    deploy: 1c53b136d119a2bc9d07165b0c29eba04027ab13

---
+++ _static/static/1654007972/projects/maori-hill-house-3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1654007972",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fmaori-hill-house-3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
