2ab529e405c58734583958d1b55bf1d3ded462b6
lowrise <lowrise@users.noreply.github.com>
2023-08-26 11:17:54 +0000

    Deploying to gh-pages from @ lowrise/lowrise_web@5f416c7cf652e5d0196d29b691a3ee93112c2bec 🚀

--- _static/static/1670409292/projects/sliver-hair-salon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1670409292",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fsliver-hair-salon",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
---
+++ _static/static/1693048671/projects/purakanui-house/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_static\u002Fstatic\u002F1693048671",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fpurakanui-house",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_static\u002F",cdnURL:a}}}}(null));
